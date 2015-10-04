378eb6968dc5bfd52c5eca7bf171dc7f196eb576
Seiei Miyagi <hanachin@gmail.com>
2015-10-05 01:26:13 +0900

    fade out light in 8pm - 12:00am

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,6 +2,12 @@ set :output, '/home/sei/yamamayaa/log/cron_log.log'
 
 job_type :irkit, 'cd :path && `ruby -e "print Gem.user_dir"`/bin/bundle exec irkit --post :task :output'
 
-every 1.minute do
-  irkit 'light_down'
+(8..11).each do |h|
+  every 1.day, at: "#{h}pm" do
+    irkit 'light_down'
+  end
+end
+
+every 1.day, at: '12:00am' do
+  irkit 'light_off'
 end
