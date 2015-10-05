65b7ddc60f2c872aa0d8eacd0653eafca217a514
Seiei Miyagi <hanachin@gmail.com>
2015-10-06 01:40:51 +0900

    WIP: Add ika notification

---
+++ bin/ika.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -e
+
+script_dir=$(cd $(dirname $0) && pwd)
+ika_path=$(dirname $script_dir)/tmp/ika
+
+ping_responses=$(ping -c 2 -i 10 -b 255.255.255.255)
+
+wii_u_ip_address=$(arp -e | grep $(cat ~/.wii_u_mac_address) | cut -f 1 -d' ')
+
+wii_u_alive=$(echo $ping_responses | grep -F $wii_u_ip_address | cat)
+
+if [ -z "$wii_u_alive" ]; then
+  if [ -f $ika_path ]; then
+    rm $ika_path
+    echo $(date) ika end
+    exit 0
+  fi
+fi
+
+if [ ! -f $ika_path ]; then
+  touch $ika_path
+  # TODO yo notification
+  echo $(date) ika start
+fi
--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,7 @@
 set :output, '/home/sei/yamamayaa/log/cron_log.log'
 
 job_type :irkit, 'cd :path && `ruby -e "print Gem.user_dir"`/bin/bundle exec irkit --post :task :output'
+job_type :yamamayaa_command, 'cd :path && :task :output'
 
 (8..11).each do |h|
   every 1.day, at: "#{h}pm" do
@@ -11,4 +12,8 @@ end
 every 1.day, at: '12:00am' do
   irkit 'light_off'
   command 'sudo ntpd -qg'
+end
+
+every 5.minutes do
+  yamamayaa_command './bin/ika.sh'
 end
