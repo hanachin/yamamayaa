132774cc878fa76c08dbdd86722638094dc245cb
Seiei Miyagi <hanachin@gmail.com>
2015-10-06 12:12:09 +0900

    Update bin/ika.sh

--- bin/ika.sh
+++ bin/ika.sh
@@ -9,9 +9,9 @@ ping_responses=$(ping -c 2 -i 10 -b 192.168.1.255)
 
 wii_u_ip_address=$(arp -e | grep $(cat ~/.wii_u_mac_address) | cut -f 1 -d' ')
 
-wii_u_alive=$(echo $ping_responses | grep -F $wii_u_ip_address | cat)
+wii_u_alive=$(echo $ping_responses | grep -F "$wii_u_ip_address")
 
-if [ -z "$wii_u_alive" ]; then
+if [ -n "$wii_u_ip_address" -a -z "$wii_u_alive" ]; then
   if [ -f $ika_path ]; then
     rm $ika_path
     echo $(date) ika end
