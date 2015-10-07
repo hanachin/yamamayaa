fc4c18886e22d4dc8c6c8de82b7489d9371fb740
Seiei Miyagi <hanachin@gmail.com>
2015-10-07 21:13:47 +0900

    Update bin/ika.sh, use nmap with sudo

--- bin/ika.sh
+++ bin/ika.sh
@@ -5,13 +5,9 @@ set -e
 script_dir=$(cd $(dirname $0) && pwd)
 ika_path=$(dirname $script_dir)/tmp/ika
 
-ping_responses=$(ping -c 2 -i 10 -b 192.168.1.255)
+wii_u_alive=$(sudo nmap -sP 192.168.1.1-16 | grep -i -F $(cat ~/.wii_u_mac_address) | cat)
 
-wii_u_ip_address=$(arp -e | grep $(cat ~/.wii_u_mac_address) | cut -f 1 -d' ')
-
-wii_u_alive=$(echo $ping_responses | grep -F "$wii_u_ip_address" | cat)
-
-if [ -z "$wii_u_ip_address" -o -z "$wii_u_alive" ]; then
+if [ -z $wii_u_alive ]; then
   if [ -f $ika_path ]; then
     rm $ika_path
     echo $(date) ika end
