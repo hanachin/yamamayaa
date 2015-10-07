3a96832ed65a07ee82caeb601c0274cde64dc770
Seiei Miyagi <hanachin@gmail.com>
2015-10-07 22:49:09 +0900

    Update bin/ika.sh

--- bin/ika.sh
+++ bin/ika.sh
@@ -7,7 +7,7 @@ ika_path=$(dirname $script_dir)/tmp/ika
 
 wii_u_alive=$(sudo nmap -sP 192.168.1.1-16 | grep -i -F $(cat ~/.wii_u_mac_address) | cat)
 
-if [ -z $wii_u_alive ]; then
+if [ -z "$wii_u_alive" ]; then
   if [ -f $ika_path ]; then
     rm $ika_path
     echo $(date) ika end
